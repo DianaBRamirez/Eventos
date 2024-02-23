ffc04e27999e4617d3570abfc9810e803041be32
DianaBRamirez <dianabramirez29@gmail.com>
2024-02-22 19:48:49 -0600

    eventos

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+
+$('#eClick').on("click", function () {
+    alert("Evento onclick");
+});
+$('#eDblclick').on("dblclick", function () {
+    alert("Evento ondblclick");
+});
+$('#eMousedown').on("mousedown", function () {
+    alert("Evento onmousedown");
+});
+$('#eMouseenter').on("mouseenter", function () {
+    alert("Evento onmouseenter");
+});
+$('#eMouseleave').on("mouseleave", function () {
+    alert("Evento onmouseleave");
+});
+$('#eMousemove').on("mousemove", function () {
+    alert("Evento onmousemove");
+});
+$('#eMouseover').on("mouseover", function () {
+    alert("Evento onmouseover");
+});
+$('#eMouseout').on("mouseout", function () {
+    alert("Evento onmouseout");
+});
+$('#eMouseup').on("mouseup", function () {
+    alert("Evento onmouseup");
+});
+$('#btnOM').on("click", function () {
+    $('#scrollUpBtn, #scrollDownBtn').toggle();
+});
+$('#scrollUpBtn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+});
+$('#scrollDownBtn').click(function () {
+    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+});
+
